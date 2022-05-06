13821cd52a66665b007c3275da179fbf171d2a13
ADMIN <maximmajor4us@gmail.com>
2022-05-06 16:00:55 +0100

    implementation of the booking form and response message

--- node_modules/react-native-floating-label-input/index.tsx
+++
@@ -1,4 +0,0 @@
-import FloatingLabelInput, { setGlobalStyles, CustomLabelProps, Props as FloatingLabelProps, SetGlobalStyles as SetGlobalStylesProps } from './src';
-
-export { FloatingLabelInput, setGlobalStyles };
-export type {CustomLabelProps, FloatingLabelProps, SetGlobalStylesProps };
